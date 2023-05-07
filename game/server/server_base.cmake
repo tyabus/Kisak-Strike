211463b20cbe38da1681eb3e58f574a0aefebb7d
tyabus <tyabustest@gmail.com>
2023-05-08 01:39:04 +0600

    server: Include vgui_controls on non dedicated, launcher_main: compile .rc on windows

--- game/server/server_base.cmake
+++ game/server/server_base.cmake
@@ -513,8 +513,14 @@ target_sources(${OUTBINNAME} PRIVATE "${SRCDIR}/common/CegClientWrapper.cpp")
 
 
 target_link_libraries(${OUTBINNAME} bonesetup_client choreoobjects_client mathlib_client mathlib_extended_client libtier0_client vstdlib_client interfaces_client particles_client)
-#target_link_libraries(${OUTBINNAME} bitmap_client dmxloader_client tier1_client tier2_client tier3_client vgui_controls_client responserules_runtime_client kisak_gcsdk_client)
-target_link_libraries(${OUTBINNAME} bitmap_client dmxloader_client tier1_client tier2_client tier3_client responserules_runtime_client kisak_gcsdk_client)
+
+# we must link in the right order
+if( DEDICATED )
+	target_link_libraries(${OUTBINNAME} bitmap_client dmxloader_client tier1_client tier2_client tier3_client responserules_runtime_client kisak_gcsdk_client)
+else()
+	target_link_libraries(${OUTBINNAME} bitmap_client dmxloader_client tier1_client tier2_client tier3_client vgui_controls_client responserules_runtime_client kisak_gcsdk_client)
+endif()
+
 #Requires evil proprietary link to libsteam_api
 if( MSVC AND CMAKE_SIZEOF_VOID_P EQUAL 4 )
 	target_link_libraries(${OUTBINNAME} ${LIBPUBLIC}/steam_api.lib Winmm.lib)
