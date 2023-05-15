ff0e3858919e7430ea934a0f2e00442d4d61bbe9
tyabus <tyabustest@gmail.com>
2023-05-15 18:26:30 +0600

    [ci skip] cmake: Add osx definitions and stuff

--- cmake/source_dll_base.cmake
+++ cmake/source_dll_base.cmake
@@ -2,17 +2,17 @@ include("${CMAKE_MODULE_PATH}/common_functions.cmake")
 include("${CMAKE_MODULE_PATH}/platform_dirs.cmake") #defines PLATSUBDIR
 include("${CMAKE_MODULE_PATH}/source_base.cmake")
 
-MacroRequired(OUTDLLEXT ".so")
-
 add_definitions(-DCSTRIKE15)
 
-if(POSIX)
-    include("${CMAKE_MODULE_PATH}/source_dll_posix_base.cmake")
-elseif(WIN32)
-	set(OUTDLLEXT ".dll")
-    include("${CMAKE_MODULE_PATH}/source_dll_posix_base.cmake")
-else(NOT OSX)
-    message(FATAL_ERROR "OS Unsupported!")
+if(WIN32)
+    set(OUTDLLEXT ".dll")
+elseif(OSXALL)
+    set(OUTDLLEXT ".dylib")
+elseif(LINUXALL)
+    set(OUTDLLEXT ".so")
 endif()
 
+MacroRequired(OUTDLLEXT ".so")
+
+include("${CMAKE_MODULE_PATH}/source_dll_posix_base.cmake")
 include("${CMAKE_MODULE_PATH}/source_video_base.cmake")
