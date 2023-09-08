4a2cb2d4c0fe58c692c6511a2c66ad760293fb91
r-a-sattarov <r.a.sattarov@yandex.ru>
2023-09-09 01:24:39 +0300

    thirdparty/protobuf-3.5.1: added e2k support (MCST Elbrus 2000)

--- thirdparty/protobuf-3.5.1/src/google/protobuf/stubs/platform_macros.h
+++ thirdparty/protobuf-3.5.1/src/google/protobuf/stubs/platform_macros.h
@@ -79,6 +79,9 @@
 #elif defined(__PPC__)
 #define GOOGLE_PROTOBUF_ARCH_PPC 1
 #define GOOGLE_PROTOBUF_ARCH_32_BIT 1
+#elif defined(__e2k__)
+#define GOOGLE_PROTOBUF_ARCH_E2K 1
+#define GOOGLE_PROTOBUF_ARCH_64_BIT 1
 #elif defined(__GNUC__)
 # if (((__GNUC__ == 4) && (__GNUC_MINOR__ >= 7)) || (__GNUC__ > 4))
 // We fallback to the generic Clang/GCC >= 4.7 implementation in atomicops.h
